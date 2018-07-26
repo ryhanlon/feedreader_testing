85fa25b351ef23c7af89cf2011e59bd39c91e09a
ryhanlon <ryhanlon@gmail.com>
2018-07-25 20:31:53 -0700

    test: RSS Feeds test suite
    Three specs testing the contents of the allFeeds array.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,13 +31,24 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-
+        it('URL is defined and not empty', function() {
+			allFeeds.forEach(function(key) {
+				expect(key.url).toBeDefined();
+				expect(key.url.length).not.toBe(0);
+			})
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('named is defined and not empty', function() {
+			allFeeds.forEach(function(key) {
+				expect(key.name).toBeDefined();
+				expect(key.name.length).not.toBe(0);
+			})
+        });
     });
 
 
