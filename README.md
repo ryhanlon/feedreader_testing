3e3793d701b5a5ffaa3aa4c692f89bb5032ef4ea
ryhanlon <ryhanlon@gmail.com>
2018-08-03 17:49:45 -0700

    tests: Initial Entries and New Feed Selections
    Refactored the The Menu Suite, not using 'spies'.

--- README.md
+++ README.md
@@ -7,36 +7,30 @@
 
 *Feed Reader* ... **detailing all steps required to successfully run the application. If you have added additional tests (for Udacious Test Coverage), provide documentation for what these future features are and what the tests are checking for.**
 
-Click [here](#picture-of-responsive-design-website) to see pictures of the website's Responsive Design.
+Click [here](#picture-of-responsive-design-website) to see screen shot of passing tests.
 
 ##### Technical Components
-- CSS Grid, Flexbox and Media Queries used to build the _responsive features_.
-- JavaScript (ECMASript 6) and jQuery 3.3.1 are used for user interactions which include:
-    -
+- Jasmine for testing
+- CSS and HTML
+- JavaScript (ECMASript 6) and jQuery 3.3.1
+
+
+##### Articles and Tutorials
+
+- FEND P5: Feed Reader Project-Walk Through with Alain [here]("https://www.youtube.com/watch?v=pPt4oOKNdEk")
+I wanted to view this video for help with the Async part of the testing and hearing further explanation using Jasmine for testing.  I originally found a more complex way to test the menu using *spies* which focus on the click event.  I changed it after watching this tutorial because I like the simpler more direct method of testing the class. I included the links for the articles on *spies* after this because they did influence my understanding of Jamsine and testing.
+
+- stack overflow solution that referenced HTMLGoodies article above, explained how to handle DOM elements in Jasime [here]("https://stackoverflow.com/questions/48872864/testing-for-click-event-with-jasmine")
 
 - article *Testing DOM Events Using jQuery and Jasmine*,  from HTMLGoodies [here]("https://www.htmlgoodies.com/beyond/javascript/js-ref/testing-dom-events-using-jquery-and-jasmine-2.0.html")
-- stack overflow solution that referenced HTMLGoodies article above, explained how to handle DOM elements in Jasime [here]("https://stackoverflow.com/questions/48872864/testing-for-click-event-with-jasmine")
-- almost used this snippet from Zakaria at stack overflow [here]("https://stackoverflow.com/questions/41514126/how-to-write-jasmine-test-case-for-checking-toggle-class-functionality-inside-cl")
 
 
 
+#### Pictures of Passed Tests
 
-#### Pictures of Responsive Design of Game
-##### Phone
+![passed tests](file path)
 
-![choose game](file path)
 
-![playing game]()
-
-##### Tablet
-![choose game]()
-
-![playing game]()
-
-##### Desk Top
-![choose game]()
-
-![playing game]()
 
 
 ### Future Features
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -4,10 +4,7 @@
  * all of the tests that will be run against your application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
- */
+
 $(function() {
     /* This is our first test suite - a test suite just contains
     * a related set of tests. This suite is all about the RSS
@@ -31,7 +28,7 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-        it('URL is defined and not empty', function() {
+        it('url is defined and not empty', function() {
 			allFeeds.forEach(function(key) {
 				expect(key.url).toBeDefined();
 				expect(key.url.length).not.toBe(0);
@@ -51,72 +48,71 @@ $(function() {
         });
     });
 
-
-    /* TODO: Write a new test suite named "The menu" */
+	// Checks that when the icon/hamburger is clicked that the menu appears and disappears
 	describe('The Menu', function() {
-		// Add a spyOnEvent, for clicks
-		let spyEvent, menu;
+		let bodyElement = $('body');
+		let hamburger = $('.menu-icon-link');
 
-		beforeEach(function() {
-			// menu icon is passed onto a spy listener
-			spyEvent = spyOnEvent('.menu-icon-link', 'click');
-		});
-
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
         it('menu is hidden by default', function() {
-        	let element = $('body');
-        	element.addClass('menu-hidden');
-				expect(element).toHaveClass('menu-hidden');
-				// expect(element.attr('class')).toEqual('menu-hidden');
+        	// When page loads, menu is hidden
+        	bodyElement.addClass('menu-hidden');
+				expect(bodyElement.hasClass('menu-hidden')).toBe(true);
 		});
 
-
-
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
-       it('menu appears when icon clicked', () => {
-
-        	// icon clicked
-			$('.menu-icon-link').trigger('click');
-			expect('click').toHaveBeenTriggeredOn('.menu-icon-link');
-			expect(spyEvent).toHaveBeenTriggered();
-			// check if class is empty
-			menu = $('body').attr('class');
-			expect(menu).toBe('');
-		});
-
-		it('menu disappears when icon is clicked', () => {
-			// icon clicked
-			$('.menu-icon-link').trigger('click');
-			expect('click').toHaveBeenTriggeredOn('.menu-icon-link');
-			expect(spyEvent).toHaveBeenTriggered();
-			// check if .menu-hidden class exists
-			menu = $('body').attr('class');
-			expect(menu).toBe('menu-hidden');
+		it('menu appears and disappears when icon clicked', ()=> {
+			// First click removes the class and the menu appears
+			hamburger.click();
+			expect(bodyElement.hasClass('menu-hidden')).toBe(false);
+			// Second click, returns the class so the menu disappears
+			hamburger.click();
+			expect(bodyElement.hasClass('menu-hidden')).toBe(true);
 		});
 
      });
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+	// Check that when the feed is loaded it is not empty.
+        describe('Initial Entries', () => {
+        	// Before the test, load the feed, done before each test
+			beforeEach(function(done) {
+				loadFeed(0, function () {
+					done();
+				});
+			});
 
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+        	it('a single entry within the .feed container', () => {
+        		// Selectors, parent=.feed child=.entry
+				let entries = $('.feed .entry');
+				expect(entries.length).toBeGreaterThan(0);
+			});
+		});
 
-    /* TODO: Write a new test suite named "New Feed Selection" */
 
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
+        describe('New Feed Selection', () => {
+        	// variables to store the html for both feeds
+			let firstFeed,
+			    secondFeed;
+
+			// Check the length of allFeeds is at least 2 values
+			it('another feed value exists', () => {
+				expect(allFeeds.length).toBeGreaterThan(1);
+			});
+
+        	// Loads two feeds from the allFeeds array for testing.  Done before each test.
+        	beforeEach(function(done) {
+        		// Wait till done, store the loaded feed for future testing
+				// Parameters are the index of the allFeeds array and the call-back function
+        		loadFeed(1, function () {
+					firstFeed = $('.feed').html();
+					// Wait till done, store the loaded feed for future testing
+					loadFeed(0, function() {
+						secondFeed = $('.feed').html();
+						done();
+					});
+        		});
+			});
+        	// Now two feeds are stored, check that the html content in each is different
+			it('feed section has been loaded and is different', function() {
+				expect(firstFeed).not.toBe(secondFeed);
+			});
+		});
 }());
